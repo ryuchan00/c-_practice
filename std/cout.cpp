22865a8dad2d9ad9749dfaf41117d3f61d7271c6
ryuchun00 <leokun0210@gmail.com>
2020-02-26 22:54:49 +0900

    文字列連結を行う

--- std/cout.cpp
+++ std/cout.cpp
@@ -1,10 +1,15 @@
 //
 // Created by yamakawa ryutaro on 2020/02/26.
 //
+// ここを参考にした
+// http://wisdom.sakura.ne.jp/programming/cpp/cpp2.html
 
 #include <iostream>
 
-int main(){
-    std::cout << "Kitty on your lap\n";
+using namespace std;
+
+int main() {
+    cout << 10 << ":" << 1.15 << '\n';
+    cout << "Kitty" << "on" << "your" << "lap\n";
     return 0;
 }
