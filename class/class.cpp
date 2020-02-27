2c0c57c6191885bf35347d3f5cba852daf2e6a89
ryuchun00 <leokun0210@gmail.com>
2020-02-27 09:24:32 +0900

    classについて理解し始める

---
+++ class/class.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Kitty {
+public:
+    char *str;
+} obj;
+
+int main() {
+    obj.str = "Kitty on your lap";
+    cout << obj.str;
+
+    return 0;
+}
